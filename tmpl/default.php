f4504d981c31c705ec09a20b71289b68eea7687f
Chris Vaughan <chris@cevsystems.co.uk>
2014-09-20 16:31:17 +0100

    update to allow option of ramblers.org.uk style footer RA Footer module should be in the copyright-a position

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,29 +1,33 @@
-<?php // no direct access
+<?php
+
+// no direct access
 /**
  * @module	RA Footer
  * @author	Chris Vaughan
  * @website	http://ramblers-webs.org.uk/
  * @copyright	Copyright (C) 2013 Chris Vaughan <webmaster@ramblers-webs.org.uk>. All rights reserved.
  * @license	http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
-
+ */
 defined('_JEXEC') or die('Restricted access');
-$startyear=$params->get('startyear');
-$copyrighttext=$params->get('copyrighttext');
-$ramblerswebs=$params->get('ramblerswebs');
+$startyear = $params->get('startyear');
+$copyrighttext = $params->get('copyrighttext');
+$ramblerswebs = $params->get('ramblerswebs');
+$footerstyle = $params->get('footerstyle');
 $document = JFactory::getDocument();
 $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/ramblers.css');
-
+if ($footerstyle == 1) {
+    $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/footerstyle.css');
+}
 $footer = '<div class="footer" >';
 
 // Copyright symbol
 $copyright_symbol = '&copy;';
 // Copyright year
-$current_year = date('Y');		
+$current_year = date('Y');
 
-$footer .= 'Copyright '.$copyright_symbol.' '.$startyear.'-'.$current_year.' '.$copyrighttext.'<br />';
-if ($ramblerswebs!=0) {
-	$footer .= 'Hosted by <a href="http://www.ramblers-webs.org.uk/" target="_blank">www.ramblers-webs.org.uk</a>. Centrally funded hosting for Areas and Groups<br />';
+$footer .= 'Copyright ' . $copyright_symbol . ' ' . $startyear . '-' . $current_year . ' ' . $copyrighttext . '<br />';
+if ($ramblerswebs != 0) {
+    $footer .= 'Hosted by <a href="http://www.ramblers-webs.org.uk/" target="_blank">www.ramblers-webs.org.uk</a>. Centrally funded hosting for Areas and Groups<br />';
 }
 $footer .= "The Ramblers' Association is a company limited by guarantee, registered in England and Wales. Company registration number: 4458492. Registered Charity in England and Wales number: 1093577. <br />Registered office: 2nd floor, Camelford House, 87-90 Albert Embankment, London SE1 7TW.";
 
@@ -34,5 +38,4 @@ $footer.='</div>';
 echo $footer;
 
 // end
-
- ?>
+?>
