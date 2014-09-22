988b25bc969d8610ba511a3497b2b7e2686c41a3
Chris Vaughan <chris@cevsystems.co.uk>
2014-09-22 09:38:23 +0100

    addition of options on Ramblers style of footer

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,10 +14,30 @@ $copyrighttext = $params->get('copyrighttext');
 $ramblerswebs = $params->get('ramblerswebs');
 $footersize = $params->get('footersize');
 $footerstyle = $params->get('footerstyle');
+$background_color = $params->get('background_color');
+$custom_background_color = $params->get('custom_background_colour_value');
+$textcolour = $params->get('textcolour');
+$standardimage = $params->get('standardimage');
+$footer_image = $params->get('footer_image');
+$imageposition = $params->get('imageposition');
 $document = JFactory::getDocument();
 $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/ramblers.css');
 if ($footerstyle == 1) {
-    $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/footerstyle.css');
+    if ($standardimage == 1) {
+        $url = "modules/mod_rafooter/images/footer-bg.png";
+    } else {
+        $url = $footer_image;
+    }
+    $text = "div#rt-bottom {background: url(" . $url . ") no-repeat scroll bottom ".$imageposition."; background-size:contain; min-height: 120px}";
+    $backcolour = getFooterColor($background_color, $custom_background_color);
+    $text.="#rt-footer,#rt-copyright {background-color: " . $backcolour . "   !important;}";
+    $text.="#rt-copyright{font-size: 0.9em !important ;}";
+    $document->addStyleDeclaration($text);
+    if ($textcolour == 0) {
+        $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/footerwhitestyle.css');
+    }else {
+          $document->addStyleSheet(JURI::base() . 'modules/mod_rafooter/css/footerdarkstyle.css');
+    }
 }
 
 // Copyright symbol
@@ -41,6 +61,55 @@ if ($footersize == 1) {
 $footer.='</div>';
 
 echo $footer;
+
+function getFooterColor($option, $customvalue) {
+    $color = '';
+    switch ($option) {
+        case "darkgrey" :
+            $color = '#333333';
+            break;
+        case "pantone0110" :
+            $color = '#D7A900';
+            break;
+        case "pantone0159" :
+            $color = '#C75B12';
+            break;
+        case "pantone0186" :
+            $color = '#C60C30';
+            break;
+        case "pantone0555" :
+            $color = '#206C49';
+            break;
+        case "pantone0583" :
+            $color = '#A8B400';
+            break;
+        case "pantone1815" :
+            $color = '#782327';
+            break;
+        case "pantone4485" :
+            $color = '#5B491F';
+            break;
+        case "pantone5565" :
+            $color = '#8BA69C';
+            break;
+        case "pantone7474" :
+            $color = '#007A87';
+            break;
+        case "white" :
+            $color = '#FFFFFF';
+            break;
+        case "custom" :
+            $color = $customvalue;
+            break;
+        case "transparent" :
+            $color = '';
+            break;
+        default:
+            $color = $customvalue;
+            break;
+    }
+    return $color;
+}
 
 // end
 ?>
